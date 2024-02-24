329d4e700c1b1bca27414901fb9b5a3a77c36ad1
DEOKJAE KWON <juintin@kakao.com>
2024-02-25 01:19:04 +0900

    feat: add fake user for test

--- club/src/main/java/org/zerock/club/config/SecurityConfig.java
+++ club/src/main/java/org/zerock/club/config/SecurityConfig.java
@@ -3,8 +3,11 @@ package org.zerock.club.config;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @Log4j2
@@ -13,5 +16,15 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password("$2a$10$9ke47d0H0huQDBOm3phf2uDWi0PqQjGf1iOcAhXgtRs2IMqZQAXGm")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 }
