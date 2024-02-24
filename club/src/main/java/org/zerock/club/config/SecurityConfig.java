392aaefd57d8caf215a39bd2471a4c9b37405d46
DEOKJAE KWON <juintin@kakao.com>
2024-02-24 22:59:49 +0900

    feat: add SecurityConfig and PasswordTests

---
+++ club/src/main/java/org/zerock/club/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package org.zerock.club.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@Log4j2
+public class SecurityConfig {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
