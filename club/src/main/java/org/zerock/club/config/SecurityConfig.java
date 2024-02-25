83103cfe7ec5241661e89ae44a0fd4e657ebb8e6
DEOKJAE KWON <juintin@kakao.com>
2024-02-25 18:09:54 +0900

    feat: google login logic

--- club/src/main/java/org/zerock/club/config/SecurityConfig.java
+++ club/src/main/java/org/zerock/club/config/SecurityConfig.java
@@ -3,14 +3,18 @@ package org.zerock.club.config;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.zerock.club.security.handler.ClubLoginSuccessHandler;
 
 @Configuration
 @Log4j2
+@EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
     @Bean
@@ -28,7 +32,13 @@ public class SecurityConfig {
         http.formLogin(config -> {});
         http.csrf(AbstractHttpConfigurer::disable);
         http.logout(config -> {});
-        http.oauth2Login(config -> {});
+        http.oauth2Login(config -> {
+            config.successHandler(successHandler());
+        });
         return http.build();
+    }
+
+    private AuthenticationSuccessHandler successHandler() {
+        return new ClubLoginSuccessHandler(passwordEncoder());
     }
 }
